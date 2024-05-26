04d8871d8a9b3bfb93861ff8f597df5de0df9124
datletu <quocdatletu@gmail.com>
2024-05-26 16:37:37 +0800

    make user guide a pop up

--- horus/src/userGuide.js
+++ horus/src/userGuide.js
@@ -1,9 +1,10 @@
 document.getElementById("userGuideHyperlink").addEventListener('click', function(event) {
     event.preventDefault();
-    var userGuideText = document.getElementById("userGuideText");
-    if (userGuideText.style.display === 'none') {
-        userGuideText.style.display = 'block';
-    } else {
-        userGuideText.style.display = 'none';
-    }
+    const userGuidePopup = document.getElementById('userGuidePopup');
+    const modalClose = userGuidePopup.querySelector('.modal-close');
+    userGuidePopup.classList.add('is-active');
+    modalClose.addEventListener('click', () => {
+        userGuidePopup.classList.remove('is-active');
+    });
 });
+
