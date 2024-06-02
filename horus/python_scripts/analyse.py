064d49b72eee0e083779943bc0f2fec0ae36415d
thegrimbee <gabriel.mario.antaputra@gmail.com>
2024-06-02 23:11:57 +0800

    Updated analyse now uses ai

--- horus/python_scripts/analyse.py
+++ horus/python_scripts/analyse.py
@@ -1,12 +1,29 @@
 import sys
-from ai import AI
+import pickle
+import os
+from textblob import TextBlob
 
-ai = AI()
+def extract_features(sentence):
+    blob = TextBlob(sentence)
+    sentiment = blob.sentiment.polarity
+    subjectivity = blob.sentiment.subjectivity
+    return [sentiment, subjectivity]
+
+def predict(sentence):
+    # Load the model from the file
+    model_path = os.path.join(os.path.dirname(__file__), '../ai_models/model2.pkl')
+    with open(model_path, 'rb') as file:
+        model = pickle.load(file)
+
+    # Predict on the test set
+    extsentence = extract_features(sentence)
+    return model.predict([extsentence])[0]
+
 def analyse_tos(tos):
     sentences = tos.split('.')
     categorized_sentences = [[], [], []]
     for sentence in sentences:
-        categorized_sentences[ai.getHarmLevel(sentence)].append(sentence)
+        categorized_sentences[predict(sentence)].append(sentence)
     return categorized_sentences
 
 if __name__ == '__main__':
