48a306ea17b15ce6d9984fbd92adcab8844b355d
thegrimbee <gabriel.mario.antaputra@gmail.com>
2024-06-14 22:08:11 +0800

    Update ai, added different classifiers which might be better

--- ai_development/ai2.py
+++ ai_development/ai2.py
@@ -1,5 +1,5 @@
 from data import get_data
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier,  GradientBoostingClassifier
 from imblearn.over_sampling import SMOTE
@@ -11,6 +11,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.feature_selection import SelectFromModel
 from textblob import TextBlob
 from sentence_transformers import SentenceTransformer
+from imblearn.over_sampling import ADASYN
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
 import numpy as np
 import pickle
 import os
@@ -49,22 +52,22 @@ def custom_loss(y_true, y_pred):
         if true == 2 and pred == 1:
             score += 4
         elif true == 2 and pred == 0:
-            score -= 30
+            score -= 25
         elif true == 1 and pred == 2:
             score += 4
         elif true == 1 and pred == 0:
-            score -= 3.5
+            score -= 3.2
         elif true == 0 and pred == 1:
             score -= 4
         elif true == 0 and pred == 2:
-            score -= 15
+            score -= 13
         elif true == pred:
             score += 10 # keep this constant
     return score
 
 custom_scorer = make_scorer(custom_loss, greater_is_better=True)
-    
-train_data, val_data = train_test_split(get_data(), test_size=0.2)
+data = get_data()
+train_data, val_data = train_test_split(data, test_size=0.2, stratify=data['Harm Level'])
 # Initialize the model
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 
@@ -78,15 +81,16 @@ Y_test = val_data['Harm Level']
 
 pipeline = ImbPipeline([
     ('features', FeatureUnion([
-        ('tfidf', TfidfVectorizer(ngram_range=(1, 2), stop_words='english', max_df=0.5, min_df=3)),
+        ('tfidf', TfidfVectorizer(ngram_range=(1, 2), stop_words='english', max_df=0.5, min_df=5)),
         ('sentence_transformer', SentenceTransformerFeatures()),
         #('textblob', TextBlobFeatures()),
     ])),
-    ('smote', SMOTE()), 
+    # ('smote', SMOTE(sampling_strategy='auto')),
+    ('adasyn', ADASYN(sampling_strategy='auto')),
     ('scaler', StandardScaler(with_mean=False)),
     ('feature_selection', SelectFromModel(GradientBoostingClassifier())), 
     ('clf', RandomForestClassifier(
-            n_estimators=100, 
+            n_estimators=200, 
             max_depth = None,
             min_samples_split = 5,
             max_features = 'sqrt',
@@ -94,25 +98,40 @@ pipeline = ImbPipeline([
             max_leaf_nodes = 30,
             min_impurity_decrease=0.0,
             class_weight='balanced_subsample',
-            min_samples_leaf=4,
             bootstrap=True,
+            min_samples_leaf=4,
         )),
+    # ('clf', XGBClassifier(
+    #         n_estimators=200, 
+    #         max_depth=None,
+    #         learning_rate=0.1,
+    #         colsample_bytree=1.0,
+    #         subsample=0.8,
+    #     )),
+    # ('clf', LGBMClassifier(
+    #         n_estimators=200, 
+    #         max_depth=None,
+    #         learning_rate=0.05,
+    #         colsample_bytree=1.0,
+    #         subsample=0.8,
+    #         num_leaves=24,
+    #     )),
 ])
 
 # Define parameter grid for GridSearchCV
-param_grid = {
-    # 'clf__min_samples_leaf': [2, 4, 6],
-    # 'clf__bootstrap': [False, True],
-    # 'features__tfidf__min_df': [2, 3, 5],
-}
+# param_grid = {
+#     'clf__n_estimators': [100, 200, 300],
+# }
 
-# Initialize GridSearchCV
-grid_search = GridSearchCV(pipeline, param_grid, cv=5, n_jobs=-1, verbose=1, scoring=custom_scorer)
-# Train the model
-grid_search.fit(X_train, Y_train)
 
-# Get the best model
-model = grid_search.best_estimator_
+# stratified_kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+# grid_search = GridSearchCV(pipeline, param_grid, cv=stratified_kfold, n_jobs=-1, verbose=1, scoring=custom_scorer)
+# grid_search.fit(X_train, Y_train)
+# model = grid_search.best_estimator_
+# print(grid_search.best_params_)
+
+pipeline.fit(X_train, Y_train)
+model = pipeline
 
 model_path = os.path.join(os.path.dirname(__file__), '../horus/ai_models/model2.pkl')
 # Save the model to a file
@@ -123,7 +142,7 @@ with open(model_path, 'wb') as file:
 Y_pred = model.predict(X_test) 
 
 # Evaluate the model
-print(grid_search.best_params_)
+
 accuracy = accuracy_score(Y_test, Y_pred)
 print(f'Accuracy: {accuracy}')
 print('Classification Report:')
