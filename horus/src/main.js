7844002971c0c5360c1a531419e5899eef9f601d
thegrimbee <gabriel.mario.antaputra@gmail.com>
2024-07-27 10:02:50 +0800

    Fixed help bug, allowed keyboard shortcuts for alt apps, removed edit toolbar

--- horus/src/main.js
+++ horus/src/main.js
@@ -152,67 +152,12 @@ const template = [
     label: 'File',
     submenu: [
       {
-        label: 'Open',
-        accelerator: 'CmdOrCtrl+O',
-        click: () => {
-          // Add your open file logic here
-        }
-      },
-      {
-        label: 'Save',
-        accelerator: 'CmdOrCtrl+S',
-        click: () => {
-          // Add your save file logic here
-        }
-      },
-      {
         label: 'Exit',
         accelerator: 'CmdOrCtrl+Q',
         click: () => {
           app.quit();
         }
-      }
-    ]
-  },
-  {
-    label: 'Edit',
-    submenu: [
-      {
-        label: 'Undo',
-        accelerator: 'CmdOrCtrl+Z',
-        role: 'undo'
       },
-      {
-        label: 'Redo',
-        accelerator: 'Shift+CmdOrCtrl+Z',
-        role: 'redo'
-      },
-      { type: 'separator' },
-      {
-        label: 'Cut',
-        accelerator: 'CmdOrCtrl+X',
-        role: 'cut'
-      },
-      {
-        label: 'Copy',
-        accelerator: 'CmdOrCtrl+C',
-        role: 'copy'
-      },
-      {
-        label: 'Paste',
-        accelerator: 'CmdOrCtrl+V',
-        role: 'paste'
-      },
-      {
-        label: 'Select All',
-        accelerator: 'CmdOrCtrl+A',
-        role: 'selectAll'
-      }
-    ]
-  },
-  {
-    label: 'View',
-    submenu: [
       {
         label: 'Reload',
         accelerator: 'CmdOrCtrl+R',
@@ -220,6 +165,11 @@ const template = [
           mainWindow.reload();
         }
       },
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
       {
         label: 'Toggle Developer Tools',
         accelerator: 'CmdOrCtrl+Shift+I',
--- horus/src/userGuide.js
+++ horus/src/userGuide.js
@@ -1,6 +1,21 @@
+var galleryModal = new bootstrap.Modal(document.getElementById('userGuidePopup'), {
+  keyboard: true
+});
+// Event listener for when the modal is shown
+document.getElementById('userGuidePopup').addEventListener('shown.bs.modal', () => {
+  window.userGuideOpen = true;
+});
+
+// Event listener for when the modal is hidden
+document.getElementById('userGuidePopup').addEventListener('hidden.bs.modal', () => {
+  window.userGuideOpen = false;
+});
 window.electron.on('open-user-guide', () => {
-  var galleryModal = new bootstrap.Modal(document.getElementById('userGuidePopup'), {
-    keyboard: false
-  });
-  galleryModal.show();
+  if (!window.userGuideOpen){
+    window.userGuideOpen = true;
+    galleryModal.show();
+  } else {
+    galleryModal.hide();
+    window.userGuideOpen = false;
+  }
 });
